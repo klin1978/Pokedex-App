380218b9fc5e7ded6f71d136cc4405f4a226c5ad
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-03-27 19:12:37 -0500

    update script.js
    change types property to be defined as an array rather than string

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-    {name: 'Sandslash', height: 1.0, types: 'ground'}
+    {name: 'Sandslash', height: 1.0, types: ['ground']}
 ];
