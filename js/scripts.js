a8bc23983060cccc318e1129cd9f9cf4502a5926
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-04-07 23:22:23 -0500

    create for loop with if-else statement
    Display pokemon list by using document.write() within for loop. If-else statement identifies pokemon larger than 1.0m with brief text.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [
     {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
     {name: 'Sandslash', height: 1.0, types: ['ground']}
 ];
+for (let i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height <= 1.0) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+    } else if (pokemonList[i].height > 1.0) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - ' + 'Wow, that\'\s big!' + '<br>');
+    }
+}
