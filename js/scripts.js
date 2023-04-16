1fd9e1a427cfb37c6dca632e25771328e9112010
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-04-16 09:51:22 -0500

    add IIFE to pokemonList
    Wrap pokemonList in an IIFE and create new pokemonRepository to hold IIFE. Add return in the IIFE with getAll() and add() and update loop code to retrieve pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-    {name: 'Sandslash', height: 1.0, types: ['ground']}
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+        {name: 'Sandslash', height: 1.0, types: ['ground']}
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 function myLoopFunction(pokemon) {
     document.write(pokemon.name+' (height: '+pokemon.height+')'+'<br>');
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
